28dcdfd56d7a14da84e0656789f09c5b45fc029b
CasJuan <castrojuanig25@gmail.com>
2025-02-07 14:49:54 -0300

    [add] Obtener el url de las imágenes

--- src/modules/products/actions/get-products.ts
+++ src/modules/products/actions/get-products.ts
@@ -1,13 +1,17 @@
 import { tesloApi } from '@/api/tesloApi';
 import type { Product } from './interfaces/product.interfaces';
+import { getProductImageAction } from './get-product-immamge.action';
 
 export const getProductsAction = async (page: number = 1, limit: number = 10) => {
   try {
     const { data } = await tesloApi.get<Product[]>(
       `/products?limit=${limit}&offset=${page * limit}`,
     );
-    console.log(data);
-    return data;
+
+    return data.map((product) => ({
+      ...product,
+      image: product.images.map(getProductImageAction),
+    }));
   } catch (error) {
     console.log(error);
     throw new Error('Error getting products');
