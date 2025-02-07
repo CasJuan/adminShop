6cf9d6b9236b54add0cae3611f42c776244c1d65
CasJuan <castrojuanig25@gmail.com>
2025-02-07 16:10:31 -0300

    [add] Componente para la paginación

--- src/modules/commmon/components/ButtonPagination.vue
+++ src/modules/commmon/components/ButtonPagination.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex justify-center space-x-3 py-10 bg-gray-100">
     <button
-      class="flex items-center space-x-1.5 rounded-lg text-white py-1.5 px-4 py bg-blue-500 disabled:bg-gray-300"
+      class="flex items-center space-x-1.5 rounded-lg text-white py-1.5 px-4 py bg-blue-500 disabled:bg-gray-300 hover:bg-blue-600 transition-all"
     >
       <span>Anteriores</span>
       <ArrowLeftIcon />
     </button>
     <button
-      class="flex items-center space-x-1.5 rounded-lg text-white py-1.5 px-4 py bg-blue-500 disabled:bg-gray-300"
+      class="flex items-center space-x-1.5 rounded-lg text-white py-1.5 px-4 py bg-blue-500 disabled:bg-gray-300 hover:bg-blue-600 transition-all"
     >
       <span>Siguientes</span>
       <ArrowRightIcon />
@@ -18,4 +18,10 @@
 <script lang="ts">
 import ArrowRightIcon from '../../../Icons/ArrowRightIcon.vue;
 import ArrowLeftIcon from '../../../Icons/ArrowLeftIcon.vue;
+
+interface Props{
+
+}
+
+defineProps<Props>();
 </script>
