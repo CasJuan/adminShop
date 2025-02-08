72a6ca591a53311d0e5c52aed223fcc1ac8ea106
CasJuan <castrojuanig25@gmail.com>
2025-02-08 17:05:33 -0300

    [add] Pinia - AuthStore

---
+++ src/modules/auth/store/auth.store.ts
@@ -0,0 +1,50 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { AuthStatus, User } from '../interfaces';
+import { loginAction } from '../actions/login.action';
+
+export const useAuthStore = defineStore('auth', () => {
+  const authStatus = ref<AuthStatus>(AuthStatus.Cheking);
+  const user = ref<User | undefined>();
+  const token = ref('');
+
+  const login = async (email: string, password: string) => {
+    try {
+      const loginResp = await loginAction(email, password);
+      if (!loginResp.ok) {
+        return false;
+      }
+
+      user.value = loginResp.user;
+      token.value = loginResp.token;
+      authStatus.value = AuthStatus.Authenticated;
+      return true;
+    } catch (error) {
+      console.log(error);
+      return logout();
+    }
+  };
+
+  const logout = () => {
+    authStatus.value = AuthStatus.Unauthenticated;
+    user.value = undefined;
+    token.value = '';
+    return false;
+  };
+
+  return {
+    user,
+    token,
+    authStatus,
+    //Getters
+    isCheking: computed(() => authStatus.value === AuthStatus.Cheking),
+    isAuthenticated: computed(() => authStatus.value === AuthStatus.Authenticated),
+
+    //TODO: getter para saber si es Admin o no
+
+    username: computed(() => user.value?.fullName),
+
+    //Actions
+    login,
+  };
+});
